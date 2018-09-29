196f9756ce7febc72fd57c24b53318facf1c00ae
Lakshmi Praveena Rangavajhula <praveena.rangavajhula@gmail.com>
2018-09-29 13:58:47 -0500

    Add files via upload

---
+++ Linked Lists/Delete Middle Node.java
@@ -0,0 +1,24 @@
+/*
+Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+that node.
+
+EXAMPLE
+lnput:the node c from the linked list a->b->c->d->e->f
+Result: nothing is returned, but the new linked list looks like a ->b->d- >e- >f
+*/
+
+boolean delete(LinkedListNode n)
+{
+	if(n!=null || n.next!=null)
+	{
+		return false;
+	}
+	n.data=n.next.data;
+	n.next=n.next.next;
+	return true;
+}
+
+
+// This cannot be solved if the node that has to be deleted is the last node.
+// To solve this problem,we can consider making a dummy node
